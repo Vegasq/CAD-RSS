5ff642ad4e105a6cbc5a6e88ecaeac841d250248
Nikolay Yakovlev <vegasq@gmail.com>
2013-04-22 18:42:24 +0300

    + Cutted for classes + Save result to SQLite3 + Colored output + RM dead code

--- cadrss.py
+++ cadrss.py
@@ -12,43 +12,57 @@ except ImportError:
 
 import re
 import datetime
+import sqlite3
 
-class CADRss:
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+
+
+class CADB:
+    ''' SQLite abstraction '''
+    #Define
+    conn = False
+
+    def db_save(self, title, descr):
+        ''' Save if doesn't exists '''
+        print(self.OKBLUE + "[DB_SAVE]" + self.ENDC + " '%s' " % title )
+        res = self.db_cursor().execute('SELECT * FROM cad WHERE `title` = "%s" ' % (title))
+        if res.fetchone() == None:
+            self.db_cursor().execute('INSERT INTO cad(title,descr) VALUES(?,?)', (title,descr,))
+            self.conn.commit()
+
+    def db_get(self, title):
+        ''' Return result, could be None '''
+        print(self.OKBLUE + "[DB_GET]" + self.ENDC + " '%s' " % title )
+        res = self.db_cursor().execute('SELECT * FROM cad WHERE `title` = "%s" ' % (title))
+        return res.fetchone()
+
+    def db_cursor(self):
+        ''' Return coursor '''
+        if self.conn == False:
+            print(self.OKBLUE + "[DB_CURSOR]" + self.ENDC + " Creating..." )
+            self.conn = sqlite3.connect('cad.db')
+            self.cur = self.conn.cursor()
+            self.cur.execute("CREATE TABLE IF NOT EXISTS cad \
+                (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,title TEXT, descr TEXT)")
+        return self.cur
+
+
+
+class NativeDataParser:
     count = 10
     basic_url = "http://www.cad-comic.com"
     native_rss = "http://www.cad-comic.com/rss/rss.xml"
     start_from = "/cad/"
-    result = []
-    d = re.compile(r".*(\d{4}-\d{2}-\d{2}).*")
+    # d = re.compile(r".*(\d{4}-\d{2}-\d{2}).*")
     d2 = re.compile(r"Ctrl\+Alt\+Del - (?P<title>.*) \((?P<date>\d{4}-\d{2}-\d{2})\)")
-    updated = False
-
     atom = '<atom:link href="http://www.cad-comic.com/rss/" rel="self" type="application/rss+xml" />'
-
-    template = """<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
-    <author>
-        <name>Ctrl+Alt+Del</name>
-        <email>pierre@cad-comic.com</email>
-    </author>
-    <title>Ctrl+Alt+Del</title>
-    <link>http://www.cad-comic.com/</link>
-    <updated>{{ updated }}</updated>
-    <description>RSS for the popular comic Ctrl+Alt+Del</description>
-    <webMaster>pierre@cad-comic.com (Pierre-Luc Brunet)</webMaster>
-    <copyright>Copyright 2002-2013 Tim Buckley and Ctrl+Alt+Del Productions. All rights reserved.</copyright>
-    <language>en-us</language>
-
-    {% for item in items %}
-    <entry>
-        <updated>{{ item[3] }}</updated>
-        <pubDate>{{ item[3] }}</pubDate>
-        <title>{{ item[0] }}</title>
-        <link href="{{ item[1] }}"/>
-        <content type="html"><![CDATA[{{ item[2] }}]]></content>
-    </entry>
-    {% endfor %}
-</feed>"""
 
     def get_soup(self, request_obj):
         soup = False
@@ -62,81 +76,71 @@ class CADRss:
 
         return soup
 
-    def __init__(self):
-        self.native_soup = self.read_original_atom()
-        self.read()
-        self.merge()
-        
-    def merge(self):
-        for item in self.native_soup.find_all('item'):
-            for page in self.result:
-                ttl = str(item.find('title'))[21:-8]
-                if page[0] == ttl:
-                    desc = item.find('description')
-                    item.find('description').string = "%s" % page[2]
-                    # item.find('description').decode_contents()
+    def read_original_feed(self):
+        print(self.OKGREEN + "[NDP_READ_FEED]" + self.ENDC + " Retrieve %s" % self.native_rss )
 
-
-    def read_original_atom(self):
         native_data = requests.get(self.native_rss)
         # Python2/3 comp
         native_soup = self.get_soup(native_data)
-
         items = native_soup.find_all('item')
 
         for i in items:
-            # i = self.get_soup(i)
-            if i.find('category').text == 'Ctrl+Alt+Del':
-                pass
-            else:
+            if i.find('category').text != 'Ctrl+Alt+Del':
                 i.extract()
         return native_soup
 
-    def read(self, url=False):
+    def read_original_site(self, url=False):
         # Set url for first iter
         if url == False:
             url = self.start_from
-        open_url = "%s%s" % (self.basic_url, url)
+        print(self.OKGREEN + "[NDP_READ_SITE]" + self.ENDC + " Retrieve %s" % url )
 
         # Send request
-        data = requests.get(open_url)
-        try:
-            soup = BeautifulSoup(data.text, "xml")
-        except AttributeError:
-            soup = BeautifulSoup(data.content, "xml")
-
+        soup = self.get_soup(requests.get("%s%s" % (self.basic_url, url)))
         
         # Save page title...
-        title = soup.find('title').text
-        d = self.d2.split(title)
-
-        # ... and extract
-        year, mon, day = d[2].split('-')
-        pubDate = datetime.datetime(int(year), int(mon), int(day), 10, 10, 10)
-        pubDate = pubDate.strftime("%a, %d %b %Y %H:%M:%S %z")
-        if not self.updated:
-            # Save last update date
-            self.updated = pubDate
-
-        # Biggest div
-        cont = soup.find(id='content')
+        d = self.d2.split(soup.find('title').text)
 
         # URL to next comics
         back = soup.find_all("a", text="Back")
         next_url = back[0].attrs['href']
 
         # Image
-        i = cont.find('img')
+        i = soup.find(id='content').find('img')
         html = i.encode('ascii').decode('utf-8')
 
-        self.result.append((d[1], open_url, html, pubDate))
+        # Save to DB
+        self.db_save(d[1], html)
 
         self.count -= 1
         if not self.count:
             return True
-        return self.read(next_url)
+        return self.read_original_site(next_url)
+
+
+
+class CADRss(CADB, NativeDataParser, Colors):
+    def __init__(self):
+        self.native_soup = self.read_original_feed()
+        # self.read_original_site()
+        self.merge()
+
+
+    def merge(self):
+        print(self.WARNING + "[CAD_RSS]" + self.ENDC + " Comparing DB with feed..." )
+        for item in self.native_soup.find_all('item'):
+            ttl = str(item.find('title'))[21:-8]
+            row = self.db_get(ttl)
+            if row != None:
+                item.find('description').string = "%s" % row[2]
+            else:
+                self.read_original_site()
+                row = self.db_get(ttl)
+                item.find('description').string = "%s" % row[2]
 
     def render(self, fl):
+        print(self.WARNING + "[CAD_RSS]" + self.ENDC + " Saving result..." )
+
         txt = self.native_soup.prettify().replace('<channel>', '<channel>\n%s' % self.atom)
         txt = txt.replace("<description>", "<description>\n<![CDATA[ ")
         txt = txt.replace("</description>", "]]>\n</description>")
